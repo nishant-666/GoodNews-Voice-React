24a21a63c0815f318730517f33550604618aa4b2
nishant666 <nishants440@gmail.com>
2020-08-15 07:54:35 +0530

    Updates

---
+++ precache-manifest.f32787c704c6fea6ec306bf79d7d983f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c9e3c61f0387c44202d7f369822a6e",
+    "url": "/GoodNews-Voice-React/index.html"
+  },
+  {
+    "revision": "f07e95f730425249d0d3",
+    "url": "/GoodNews-Voice-React/static/css/2.06a7e8f7.chunk.css"
+  },
+  {
+    "revision": "1ad00e3e29a7b4836239",
+    "url": "/GoodNews-Voice-React/static/css/main.79aeeedd.chunk.css"
+  },
+  {
+    "revision": "f07e95f730425249d0d3",
+    "url": "/GoodNews-Voice-React/static/js/2.5425b25c.chunk.js"
+  },
+  {
+    "revision": "4a1691fd44116c5f8c99a494e6966d73",
+    "url": "/GoodNews-Voice-React/static/js/2.5425b25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad00e3e29a7b4836239",
+    "url": "/GoodNews-Voice-React/static/js/main.16b9c1d8.chunk.js"
+  },
+  {
+    "revision": "191933c1ee7975e08340",
+    "url": "/GoodNews-Voice-React/static/js/runtime-main.3a6b58a9.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/GoodNews-Voice-React/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/GoodNews-Voice-React/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/GoodNews-Voice-React/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/GoodNews-Voice-React/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/GoodNews-Voice-React/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/GoodNews-Voice-React/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/GoodNews-Voice-React/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/GoodNews-Voice-React/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/GoodNews-Voice-React/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/GoodNews-Voice-React/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/GoodNews-Voice-React/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/GoodNews-Voice-React/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/GoodNews-Voice-React/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/GoodNews-Voice-React/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/GoodNews-Voice-React/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/GoodNews-Voice-React/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
